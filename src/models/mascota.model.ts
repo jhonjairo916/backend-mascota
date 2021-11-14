22ab25f0b7481082837acb3864e93097e6ac1961
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-11-13 22:44:26 -0500

    Inicio de creacion de modelo mascota

---
+++ src/models/mascota.model.ts
@@ -0,0 +1,58 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Mascota extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  activo: boolean;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  especie: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  raza: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  color: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha_nacimiento: string;
+
+
+  constructor(data?: Partial<Mascota>) {
+    super(data);
+  }
+}
+
+export interface MascotaRelations {
+  // describe navigational properties here
+}
+
+export type MascotaWithRelations = Mascota & MascotaRelations;
