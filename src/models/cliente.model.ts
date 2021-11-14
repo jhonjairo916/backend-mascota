e390d087ed8745fc6d38cf082324854fe23b3694
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-11-13 22:53:53 -0500

    Creacion de modelo cliente

---
+++ src/models/cliente.model.ts
@@ -0,0 +1,47 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Cliente extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  apellido: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  ciudad: string;
+
+
+  constructor(data?: Partial<Cliente>) {
+    super(data);
+  }
+}
+
+export interface ClienteRelations {
+  // describe navigational properties here
+}
+
+export type ClienteWithRelations = Cliente & ClienteRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './mascota.model';
+export * from './cliente.model';
