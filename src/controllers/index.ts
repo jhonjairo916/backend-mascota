e654710cec60daf855d0d914810ded3da3908cd4
jhonjairo916 <jhonjairo.916@hotmail.com>
2021-11-16 12:41:59 -0500

    Creacion de coltroladores para cliente y mascota

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,5 @@
 export * from './ping.controller';
 export * from './mascota-cliente.controller';
 export * from './cliente-mascota.controller';
+export * from './cliente.controller';
+export * from './mascota.controller';
